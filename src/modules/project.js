1d7914f24360cce4fe46a88e6c13c1dc53e1de9d
Skryre <zbk1998@ymail.com>
2023-01-22 12:43:04 -0500

    class function done

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,5 +1,39 @@
 export class Projects {
     constructor(title) {
         this.title = title;
+        this.tasks = [];
+    }
+
+    setTitle(title) {
+        this.title = title
+    }
+
+    setDate(dueDate) {
+        this.dueDate = dueDate
+    }
+    
+    gettitle() {
+        return this.title
+    }
+    
+    getDate() {
+        return this.dueDate
+    }
+
+    getTask(taskTitle) {
+        return this.tasks.find((task) => task.gettitle() === taskTitle)
+      }
+    
+    contains(taskTitle) {
+        return this.tasks.some((task) => task.gettitle() === taskTitle)
+    }
+    
+    addTask(newTask) {
+        if (this.tasks.find((task) => task.gettitle() === newTask.Title)) return
+        this.tasks.push(newTask)
+    }
+    
+    deleteTask(taskTitle) {
+        this.tasks = this.tasks.filter((task) => task.title !== taskTitle)
     }
 }
--- src/modules/task.js
+++ src/modules/task.js
@@ -1,6 +1,41 @@
 export class Task {
-    constructor(title, dueDate, project, priority) {
+    constructor(title, dueDate, note, priority) {
         this.title = title;
         this.dueDate = dueDate;
+        this.note = note;
+        this.priority = priority;
     }
+    setTitle(title) {
+        this.title = title
+    }
+
+    setDate(dueDate) {
+        this.dueDate = dueDate
+    }
+
+    setNote(note){
+        this.note = note
+    }
+
+    setPriority(priority){
+        this.priority = priority
+    }
+
+    returnTitle() {
+        return this.title
+    }
+    
+    returnDate() {
+        return this.dueDate
+    }
+
+    returnNotes() {
+        return this.note
+    }
+
+
+    returnPriority() {
+        return this.priority
+    }
+    
 }
