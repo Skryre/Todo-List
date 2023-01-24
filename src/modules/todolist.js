2321c46a789677c58785325ec9b6c959b9e76790
Skryre <zbk1998@ymail.com>
2023-01-24 17:17:35 -0500

    New module for todolist array and its methods

---
+++ src/modules/todolist.js
@@ -0,0 +1,35 @@
+class TodoList {
+
+    constructor() {
+        this.projects = []
+    }
+
+    setProjects (projects) {
+        this.projects = projects
+    };
+    
+    returnProjects() {
+        return this.projects
+    };
+    
+    returnProject(projectName) {
+        return this.projects.find((project) => project.getName() === projectName)
+    }
+    
+    contains(projectName) {
+        return this.projects.some((project) => project.getName() === projectName)
+    }
+    
+    addProject(newProject) {
+        if (this.projects.find((project) => project.name === newProject.name))
+        return this.projects.push(newProject)
+    }
+    
+    deleteProject(projectName) {
+        const projectToDelete = this.projects.find(
+          (project) => project.getName() === projectName
+        )
+        this.projects.splice(this.projects.indexOf(projectToDelete), 1)
+    }
+
+}
