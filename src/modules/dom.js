ce066c26b8aee787e683b6cb6d1bf481739e16da
Skryre <zbk1998@ymail.com>
2023-01-24 21:49:44 -0500

    add website icon and some style for dark and light mode

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -2,3 +2,12 @@ import TodoList from './TodoList'
 import Project from './Project'
 import Task from './Task'
 
+export function changeBurger(x) {
+    x.classList.toggle("change");
+}
+
+export function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+}
